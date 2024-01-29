c1c9fae82c4fb66c70d39034f236d7f03874fed9
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-01-29 18:59:14 +0530

    Time: 6 ms (8.07%) | Memory: 10.2 MB (7.03%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        if(digits.empty()){
+          if (digits.empty()) {
             return {};
         }
-        vector<string> pad = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        vector<string> output;
-        output.push_back("");
-        for(auto key : digits){
-            vector<string> temp;
-            for(auto candidate : pad[key-'0']){
-                for(auto c : output){
-                    temp.push_back(c + candidate);
-                }
-            }
-            output.clear();
-            output = temp;
-        }
-        return output;
+        vector<string> res;
+        string par="";
+         unordered_map<char, string> m = {
+            {'2', "abc"},
+            {'3', "def"},
+            {'4', "ghi"},
+            {'5', "jkl"},
+            {'6', "mno"},
+            {'7', "pqrs"},
+            {'8', "tuv"},
+            {'9', "wxyz"}
+        };
+         getcom(digits,0,m,par, res);
+         return res;
+    }
+
+    void getcom(string digits, int start,  unordered_map<char, string> m,string &par, vector<string> &res){
+       
+       if (start == digits.size()){
+           res.push_back(par);
+           return;
+       }
+       string str = m[digits[start]];
+
+       for (int i=0; i<str.size(); i++){
+           
+        par.push_back(str[i]);
+        getcom(digits,start+1,m,par, res);
+        par.pop_back();
+       }
+
     }
 };
