802e96f644d38d2a02c9d421649d07db5f6fca11
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-02-02 12:03:48 +0530

    Time: 4 ms (30.24%) | Memory: 8.9 MB (67.39%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-int sum=0;
-unordered_map<int,int> robsum;
     int rob(vector<int>& nums) {
-       return getrob(nums,nums.size()-1);
-    }
-
-    int getrob(vector<int>&nums,int i){
-        if(i<0){
-            return 0;
+        int prev=0;
+        int cur=0;
+        int next=0;
+        for(int i=0; i<nums.size(); i++){
+             next = max(prev+nums[i],cur);
+             prev=cur;
+             cur=next;
         }
-        if(robsum.find(i)!=robsum.end()){
-            return robsum[i];
-        }
-        int result =  max(getrob(nums,i-2)+nums[i],getrob(nums,i-1));
-        robsum[i]=result;
-        return result;
+        return cur;
     }
 };
