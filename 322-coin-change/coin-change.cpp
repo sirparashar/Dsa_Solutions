1a4f414dc0d60a3ad4a82f642175a7801c94505b
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-02-09 00:52:52 +0530

    Time: 350 ms (5.38%) | Memory: 66.5 MB (5.92%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        unordered_map<int, int> memo;
-        int result = changePossible(coins, amount, memo);
-        return (result == INT_MAX) ? -1 : result;
-    }
 
-    int changePossible(vector<int>& coins, int amount, unordered_map<int, int>& memo) {
+        unordered_map<int, int> m;
+        return helper(coins, amount, m);
+    }
+    int helper(vector<int>& coins, int amount, unordered_map<int, int>& m) {
         if (amount == 0) {
             return 0;
         }
 
-        if (memo.find(amount) != memo.end()) {
-            return memo[amount];
+        if (amount < 0) {
+            return -1;
         }
 
-        int minCount = INT_MAX;
-
-        for (int coin : coins) {
-            if (amount >= coin) {
-                int subproblem = changePossible(coins, amount - coin, memo);
-                if (subproblem != INT_MAX) {
-                    minCount = min(minCount, subproblem + 1);
+        if (m.find(amount) != m.end()) {
+            return m[amount];
+        }
+        int mincoins = -1;
+        for (int i = 0; i < coins.size(); i++) {
+            if (amount - coins[i] >= 0) {
+                int subcoins = helper(coins, amount - coins[i], m);
+                if (subcoins != -1) {
+                    int numcoins = subcoins + 1;
+                    if (numcoins < mincoins || mincoins == -1) {
+                        mincoins = numcoins;
+                    }
                 }
             }
         }
-
-        memo[amount] = minCount;
-        return minCount;
+        m[amount] = mincoins;
+        return m[amount];
     }
 };
