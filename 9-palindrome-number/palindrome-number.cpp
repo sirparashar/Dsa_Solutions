792216b92bc2cc3b4d98eb1136fd543d275107fd
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-03-14 00:30:43 +0530

    Time: 12 ms (33.32%) | Memory: 8.9 MB (23.44%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -5,9 +5,11 @@ public:
 
         int i=0; int j=s.size()-1;
         while(i<=j){
-            if(s[i++]!=s[j--]){
+            if(s[i]!=s[j]){
                 return false;
             }
+            i++;
+            j--;
         }
         return true;
     }
