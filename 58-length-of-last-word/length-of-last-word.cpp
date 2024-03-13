60cb4eb790c91a0e6b00c673a2e677b03776404e
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-03-14 02:15:12 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (52.04%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
+
     int lengthOfLastWord(string s) {
-        int endstart=s.size();
-        for(int i=s.size()-1; i>=0; i--){
-          if(s[i]!=' '){
-             endstart =i;
-             break;
-          }
-        }
-        cout<<endstart;
-        int count = 0;
-        for(int i=endstart; i>=0; i--){
-           if(s[i]== ' '){
-            break;
-           }
-           count++;
+        int count=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if (s[i] != ' ') {
+                count++;
+            }
+            else{
+                if(count>0){
+                    return count;
+                }
+            }
+
         }
         return count;
+        
     }
 };
