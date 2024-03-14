51daa66b65810e053dc48bc136ba2d9599e7fc9a
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-03-15 02:14:20 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (69.67%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* dummy=new ListNode();
-        ListNode* curr=dummy;
-        while(list1 && list2){
-            if(list1->val < list2->val)
-               { curr->next=list1;
-                list1=list1->next;
-               }
-               else {
-                   curr->next=list2;
-                   list2=list2->next;
-               }
-               curr=curr->next;
-
-            
+        if (list1 == nullptr) {
+            return list2;
         }
-        if(list1==NULL)
-            curr->next=list2;
-        
-        else 
-        curr->next=list1;
-        return dummy->next;
+        if (list2 == nullptr) {
+            return list1;
+        }
+
+        ListNode* temp = new ListNode(); // Dummy node to start the merged list
+        ListNode* cur = temp; // Pointer to track the current node in the merged list
+
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                cur->next = list1;
+                list1 = list1->next;
+            } else {
+                cur->next = list2;
+                list2 = list2->next;
+            }
+            cur = cur->next;
+        }
+
+        // Attach remaining nodes from list1 or list2, if any
+        cur->next = list1 ? list1 : list2;
+
+        // Return the merged list, excluding the dummy node
+        ListNode* result = temp->next;
+        delete temp; // Free the memory allocated for the dummy node
+        return result;
     }
 };
