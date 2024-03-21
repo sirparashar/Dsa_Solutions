68e2b0669932a95b10fc4ad8d0fab071f8b3a9f2
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-03-21 17:18:58 +0530

    Time: 3 ms (47.39%) | Memory: 7.5 MB (69.80%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -3,18 +3,15 @@ public:
     bool isSubsequence(string s, string t) {
         int i =0;
         int j=0;
-        int count=0;
         while (i<t.size()){
             if(s[j]==t[i]){
                 j++;
-                count++;
             }
             i++;
 
         }
-        cout<<count;
 
-       return count==s.size();
+       return j==s.size();
 
     }
 };
