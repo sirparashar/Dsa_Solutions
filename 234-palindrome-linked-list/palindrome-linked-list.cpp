765ffc3b17af49e82a75b40216bc71ce358b8403
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-03-22 19:27:03 +0530

    Time: 168 ms (63.59%) | Memory: 116.6 MB (82.85%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -1,33 +1,16 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-
-        vector<int>arr;
-        while(head!=nullptr){
-           arr.push_back(head->val);
-           head=head->next;
-           cout<<arr.back();
-        }
-        
-        int i =0;
-        int j = arr.size()-1;
-        
-        while(i<=j){
-            if(arr[i++]!=arr[j--]){
-                return false;
-            }
-        }
+        ListNode *slow = head, *fast = head, *prev, *temp;
+        while (fast && fast->next)
+            slow = slow->next, fast = fast->next->next;
+        prev = slow, slow = slow->next, prev->next = NULL;
+        while (slow)
+            temp = slow->next, slow->next = prev, prev = slow, slow = temp;
+        fast = head, slow = prev;
+        while (slow)
+            if (fast->val != slow->val) return false;
+            else fast = fast->next, slow = slow->next;
         return true;
-        
     }
 };
