aa30bc226b75346b0520e8a7d0bf5360ebb3f356
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-04-20 00:40:59 +0530

    Time: 23 ms (90.28%) | Memory: 15.8 MB (67.22%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,46 +1,33 @@
 class Solution {
 public:
-    int count=0;
-    set<pair<int,int>>s;
     int numIslands(vector<vector<char>>& grid) {
-
-        for(int i=0; i<grid.size(); i++){
-            for(int j=0; j<grid[0].size(); j++){
-                if(getiland(grid,i,j)){
-                    count++;
+        if (grid.empty() || grid[0].empty()) {
+            return 0;
+        }
+        
+        int numIslands = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == '1') {
+                    numIslands++;
+                    dfs(grid, i, j);
                 }
             }
         }
-           
-           return count;
+        
+        return numIslands;
     }
-
-    bool getiland(vector<vector<char>>& grid, int i, int j){
-      
-    if(i<0 || i>grid.size()-1){
-        return false;
-    }
-    if(j<0 || j>grid[0].size()-1){
-        return false;
-    }
-
-    if(grid[i][j]=='0'){
-        return false;
-    }    
-
-    if(s.find({i,j})!=s.end()){
-        return false;
-    }
-
-    s.insert({i,j});
-
-    getiland(grid,i-1,j);
-    getiland(grid,i+1,j);
-    getiland(grid,i,j+1);
-    getiland(grid,i,j-1);
-
-    return true;
     
-
+private:
+    void dfs(vector<vector<char>>& grid, int i, int j) {
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != '1') {
+            return;
+        }
+        
+        grid[i][j] = '0'; // mark as visited
+        dfs(grid, i + 1, j); // down
+        dfs(grid, i - 1, j); // up
+        dfs(grid, i, j + 1); // right
+        dfs(grid, i, j - 1); // left
     }
 };
