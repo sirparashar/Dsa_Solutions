e77fcfb5d771a1477752c29d5ac07078a7b9ed4c
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-05-12 22:22:31 +0530

    Time: 8 ms (72.73%) | Memory: 13.5 MB (81.97%) - LeetSync

--- 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.cpp
+++ 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.cpp
@@ -1,23 +1,32 @@
 class Solution {
+
 public:
-    vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
-        int n = grid.size();
-        vector<vector<int>> res(n - 2, vector<int>(n - 2));
 
-        for(int i = 1; i < n - 1; ++i) {
-            for(int j = 1; j < n - 1; ++j) {
-                int temp = 0;
-
-                for(int k = i - 1; k <= i + 1; ++k) {
-                    for(int l = j - 1; l <= j + 1; ++l) {
-                        temp = max(temp, grid[k][l]);
-                    }
-                }
-
-                res[i - 1][j - 1] = temp;
+    int largestLocalUtil(std::vector<std::vector<int>>& grid, int x, int y) {
+        int max = 0;
+        
+        for (int i = x ; i < x+3 ; i++) {
+            for (int j = y ; j < y+3 ; j++) {
+                max = std::max(max, grid[i][j]);
             }
         }
-
-        return res;
+        
+        return max;
+    }
+    
+    vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
+        int n = grid.size();
+        
+        int m = n-2;
+        
+        std::vector<std::vector<int>> ans(m, std::vector<int>(m, 0));
+        
+        for (int i = 0 ; i < m ; i++) {
+            for (int j = 0 ; j < m ; j++) {
+                ans[i][j] = largestLocalUtil(grid, i, j);
+            }
+        }
+        
+        return ans;
     }
 };
