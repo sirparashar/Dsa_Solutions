1298cbabd4cd8be5df7470faf55e5af5412efd0f
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-05-18 21:01:31 +0530

    Time: 4 ms (63.21%) | Memory: 15.4 MB (99.33%) - LeetSync

--- 1021-distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.cpp
+++ 1021-distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.cpp
@@ -1,30 +1,19 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-        static int distributeCoins(TreeNode* root, TreeNode* parent= NULL) {
-        if (!root) return 0;
-        int  moves=distributeCoins(root->left, root)+distributeCoins(root->right, root);
-        int x=root->val-1;// give x coins to parent node
-        if (parent) parent->val += x; // now parent's coins
-        moves+=abs(x);
+    int distributeCoins(TreeNode* root) {
+        TreeNode dummy;
+        return dfs(root, &dummy);
+    }
+    
+private:
+    int dfs(TreeNode* cur_node, TreeNode* parent) {
+        if (cur_node == nullptr) {
+            return 0;
+        }
+        int moves = dfs(cur_node->left, cur_node) + dfs(cur_node->right, cur_node);
+        int from_this = cur_node->val - 1;
+        parent->val += from_this;
+        moves += abs(from_this);
         return moves;
     }
 };
-
-
-auto init = []() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
