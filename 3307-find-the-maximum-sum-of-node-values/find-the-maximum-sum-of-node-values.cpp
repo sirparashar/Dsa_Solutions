a966bcacd0dca54d61cb1aa5d8f75c1aa7a6248a
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-05-19 23:15:40 +0530

    Time: 193 ms (56.22%) | Memory: 127 MB (46.00%) - LeetSync

--- 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
+    long maximumValueSum(std::vector<int>& nums, int k, std::vector<std::vector<int>>& edges) {
         int n = nums.size();
-        vector<vector<long long>> temp(n, vector<long long>(2, -1)); // temp[current_index(node)][is_even]
-        
-        return calculateMax(nums, n, k, 0, 1, temp);
-    }
+        std::vector<int> diff(n);
+        long sum = 0;
 
-private:
-    // calculate_max -> cur_ind -> cur_index of the tree and is_even represents whether we have already changed (XOR) even or odd number of nodes 
-    long long calculateMax(vector<int>& nums, int n, int k, int curInd, int isEven, vector<vector<long long>>& temp) {
-        if (curInd == n) {  // if we go to node which doesn't exist
-            return isEven == 1 ? 0 : LLONG_MIN;
-        }
-        if (temp[curInd][isEven] != -1) {  // if we've already encountered this state
-            return temp[curInd][isEven];
+        for (int i = 0; i < n; ++i) {
+            diff[i] = (nums[i] ^ k) - nums[i];
+            sum += nums[i];
         }
 
-        // checking all possible variants (no XOR or XOR)
-        long long noXor = nums[curInd] + calculateMax(nums, n, k, curInd + 1, isEven, temp);  // we don't change the number of XOR nodes
-        long long withXor = (nums[curInd] ^ k) + calculateMax(nums, n, k, curInd + 1, !isEven, temp);  // we added 1 XORed node
+        std::sort(diff.begin(), diff.end(), std::greater<int>());
 
-        long long mxPossible = max(noXor, withXor);
-        temp[curInd][isEven] = mxPossible;
-        return mxPossible;
+        for (int i = 0; i < n; i += 2) {
+            if (i + 1 == n) return sum;
+            int pair = diff[i] + diff[i + 1];
+            if (pair > 0) sum += pair;
+        }
+
+        return sum;
     }
 };
