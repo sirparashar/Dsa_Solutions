92346438e0992bc9ef072a6da395c9091ff95898
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-06-05 01:27:12 +0530

    Time: 10 ms (16.63%) | Memory: 9.3 MB (6.85%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+
+        set<char> m;
+
+        int len = 0;
+
+        for (auto c : s) {
+            if (m.find(c) != m.end()) {
+                len += 2;
+                m.erase(c);
+            } else {
+                m.insert(c);
+            }
+        }
+
+        if(!m.empty()){
+            len+=1;
+        }
+        return len;
+    }
+};
