752f4fcd3ef3b83808cc73a1cfdd3a0ef2f5c651
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-06-06 01:23:05 +0530

    Time: 12 ms (30.01%) | Memory: 12.6 MB (27.31%) - LeetSync

--- 1044-find-common-characters/find-common-characters.cpp
+++ 1044-find-common-characters/find-common-characters.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
+    vector<int> occurence(string word)
+    {
+        vector<int> occ(26, 0);
+        
+        for (char c : word)
+            occ[c - 'a']++;
+        
+        return occ;
+    }
     vector<string> commonChars(vector<string>& words) {
-
-        vector<string> res;
-
-        for (char ch = 'a'; ch<='z'; ch++) {
-            int mincount=INT_MAX;
-            for (auto s : words) {
-                int count = 0;
-                for (char c:s) {
-                    if(c==ch){
-                        count++;
-                    }
-                }
-                mincount=min(mincount,count);
-            }
-
-            for(int i=0; i<mincount; i++){
-                res.push_back({ch});
+        vector<string> chars;
+        vector<int> occ(26, INT_MAX);
+        
+        for (string word : words)
+        {
+            auto temp = occurence(word);
+            
+            for (int i = 0; i < 26; i++)
+                occ[i] = min(occ[i], temp[i]);
+        }
+        
+        for (int i = 0; i < 26; i++)
+        {
+            char c = 'a' + i;
+            for (int j = 0; j < occ[i]; j++)
+            {
+                string s = "";
+                s += c;
+                chars.push_back(s);
             }
         }
-
-        return res;
+        
+        return chars;
     }
 };
