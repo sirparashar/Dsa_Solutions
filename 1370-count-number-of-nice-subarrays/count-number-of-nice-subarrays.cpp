5f05aa8b6c8527ebd09a9314c89d50ff0fd14044
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-06-24 02:14:01 +0530

    Time: 90 ms (73.11%) | Memory: 73.1 MB (44.25%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> cnt(n + 1, 0);
-        cnt[0] = 1;
-        int ans = 0, t = 0;
-        for (int v : nums) {
-            t += v & 1;
-            if (t - k >= 0) {
-                ans += cnt[t - k];
+        int start = 0;
+        int end = 0;
+        int count = 0;
+        int res = 0;
+        vector<int> prefix(nums.size() + 1, 0);
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] % 2 != 0) {
+                nums[i] = 1;
+            } else {
+                nums[i] = 0;
             }
-            cnt[t]++;
+        }
+        prefix[0] = 1;
+        int s = 0;
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            s += nums[i];
+            ans += (s >= k) ? prefix[s - k] : 0;
+            prefix[s]++;
         }
         return ans;
+
+        return nums[0];
     }
 };
