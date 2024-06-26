df5d61a915087258999e8f17b308055d0ac15190
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-06-27 00:49:45 +0530

    Time: 60 ms (97.88%) | Memory: 46.5 MB (91.15%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
@@ -6,40 +6,37 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
+    void inorder(TreeNode* root , vector<TreeNode*> &nodes){
+        if(root==NULL) return ;
+        inorder(root->left,nodes);
+        nodes.push_back(root);
+        inorder(root->right,nodes);
+    }
+    TreeNode* solve(vector<TreeNode*> &nodes,int start , int end){
+
+        if(end<start) return NULL;
+
+
+        //if(end==start) return nodes[start];
+
+        int mid = (end-start)/2+start;
+        TreeNode* root = nodes[mid];
+        root->left = solve(nodes,start,mid-1);
+        root->right = solve(nodes,mid+1,end);
+        return root;
+    }
+
 public:
-    void inorder(TreeNode* root, vector<int> &arr) {
-
-        if (root == nullptr) {
-            return ;
-        }
-        inorder(root->left,arr);
-        arr.push_back(root->val);
-        inorder(root->right,arr);
-
-    }
-
-    TreeNode* construct(vector<int> arr, int start, int end) {
-        if (start > end) {
-            return nullptr;
-        }
-
-        int mid = start + (end - start) / 2;
-        TreeNode* head = new TreeNode(arr[mid]);
-        head->left = construct(arr, start, mid - 1);
-        head->right = construct(arr, mid + 1, end);
-        return head;
-    }
-
     TreeNode* balanceBST(TreeNode* root) {
-        vector<int> arr;
-        inorder(root, arr);
-        cout << arr.size();
-        TreeNode* head = construct(arr, 0, arr.size() - 1);
-        return head;
+        if(root==NULL) return NULL;
+
+
+        vector<TreeNode*> inorder_traversal;
+        inorder(root,inorder_traversal);
+        return solve(inorder_traversal,0,inorder_traversal.size()-1);
     }
 };
