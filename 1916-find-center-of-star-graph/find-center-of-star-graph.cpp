bff38a0c29e3575e0a41572ccde3c221558cc249
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-06-27 18:15:40 +0530

    Time: 140 ms (50.92%) | Memory: 70.7 MB (77.19%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        vector<int>s(1);
-        sort(edges[0].begin(),edges[0].end());
-        sort(edges[1].begin(),edges[1].end());
-        set_intersection(edges[0].begin(),edges[0].end(),edges[1].begin(),edges[1].end(),s.begin());
-        return s[0];
+        return (edges[0][0]==edges[1][0] || edges[0][0]==edges[1][1] )?edges[0][0]:edges[0][1];
     }
 };
