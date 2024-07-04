846ca2a8ff9c742936d9b7c845c6353d1329ea6b
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-07-05 01:49:18 +0530

    Time: 436 ms (63.81%) | Memory: 256.5 MB (96.76%) - LeetSync

--- 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.cpp
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.cpp
@@ -1,35 +1,16 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* mergeNodes(ListNode* head) {
-
-        ListNode* dummy = new ListNode();
-        ListNode*temp=dummy;
-        head=head->next;
-
+   ListNode* mergeNodes(ListNode* head) {
+     head=head->next;
+     ListNode* start=head;
+     while(start){
+	    ListNode* end= start;   
         int sum=0;
-        while(head!=nullptr){
-            sum+=head->val;
-            if(head->val==0){
-                cout<<sum;
-                temp->next = new ListNode(sum);
-                temp=temp->next;
-                sum=0;
-            }
-            head=head->next;
-        }
-
-        return dummy->next;
-        
-        
-    }
+        while(end->val!=0) sum+= end->val , end=end->next;
+        start->val=sum;   
+        start->next=end->next;  
+        start=start->next;   
+	 }
+     return head;
+}
 };
