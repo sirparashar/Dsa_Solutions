29c79dbc6f230b97e04b4d899688220fc15bce2f
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-07-30 03:11:34 +0530

    Time: 48 ms (48.71%) | Memory: 11.7 MB (52.64%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.cpp
+++ 1511-count-number-of-teams/count-number-of-teams.cpp
@@ -1,34 +1,36 @@
 class Solution {
-    void update(vector<int> &v, int i, const int d) {
-        for (; i < v.size(); v[i] += d, i += (i & -i));
-    }
-    
-    int presum(const vector<int> &v, int i) {
-        int r = 0;
-        for (; i; r += v[i], i -= (i & -i));
-        return r;
-    }
-    
 public:
     int numTeams(vector<int>& rating) {
-        map<int, int> have;
-        for (int x : rating) {
-            have[x];
+        int res = 0;
+        for (int i = 1; i < rating.size() - 1; i++) {
+            int ls = 0, rl = 0;
+            for (int j = 0; j < i; j++) {
+                if (rating[j] < rating[i]) {
+                    ls++;
+                }
+            }
+            for (int j = i + 1; j < rating.size(); j++) {
+                if (rating[j] > rating[i]) {
+                    rl++;
+                }
+            }
+            res += ls * rl;
+
+            int ll = 0, rs = 0;
+
+            for (int j = 0; j < i; j++) {
+                if (rating[j] > rating[i]) {
+                    ll++;
+                }
+            }
+            for (int j = i + 1; j < rating.size(); j++) {
+                if (rating[j] < rating[i]) {
+                    rs++;
+                }
+            }
+
+            res += ll * rs;
         }
-        int n = 0;
-        vector<int> after(rating.size() + 1);
-        for (auto& t : have) { 
-            update(after, t.second = ++n, 1);
-        }
-        vector<int> before(n + 1);
-        int r = 0;
-        for (int i = 0; i < n; ++i) {
-            const int x = have[rating[i]], t1 = presum(before, x), t2 = presum(after, x);
-            r += t1 * (n - i - t2) + (i - t1) * (t2 - 1);
-            update(after, x, -1);
-            update(before, x, 1);
-        }
-        return r;
-        
+        return res;
     }
 };
