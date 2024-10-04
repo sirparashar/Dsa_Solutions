0b6bf0aef80d83b9cc3e0fcd4c21bb7accf028a3
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-10-05 02:24:37 +0530

    Time: 132 ms (5.44%) | Memory: 56.5 MB (56.85%) - LeetSync

---
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long dividePlayers(vector<int>& skill) {
+        sort(skill.begin(), skill.end());
+
+        vector<pair<int, int>> group;
+        if (skill.size()%2 != 0) {
+            cout<<'p';
+            return -1;
+        }
+
+        int i = 0;
+       int j = skill.size() - 1;
+        long long res=0;
+
+        long long sum = 0;
+        long long  cursum = skill[0] + skill[skill.size() - 1];
+        while (j > i) {
+            sum = skill[i] + skill[j];
+            if (sum != cursum) {
+                return -1;
+            }
+            cout<<skill[i]<<skill[j]<<"\n";
+            res += skill[i] * skill[j];
+            i++;
+            j--;
+            
+        }
+
+        return res;
+    }
+};
