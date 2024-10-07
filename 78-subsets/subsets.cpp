be9db47b5f02fd4491da31dfd1b456b1cb8a5b1d
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-10-08 02:07:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (43.91%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    void getset(vector<int>& nums, int start, vector<int> set,
-                vector<vector<int>>& res) {
- res.push_back(set);
-        for (int i = start; i < nums.size(); i++) {
-            set.push_back(nums[i]);
-            getset(nums, i + 1, set, res);
-            set.pop_back();
-        }
-       
+    
+    void getsub(vector<int> nums,int index,vector<int>& sub,  vector<vector<int>>& res ){
+       res.push_back(sub);
+
+       for(int i =index; i<nums.size(); i++){
+        sub.push_back(nums[i]);
+        getsub(nums,i+1,sub,res);
+        sub.pop_back();
+       }
+
+
     }
 
+
     vector<vector<int>> subsets(vector<int>& nums) {
-
         vector<vector<int>> res;
-        vector<int> set;
-        getset(nums, 0, set, res);
-
+        vector<int> sub;
+        getsub(nums,0,sub,res);
         return res;
     }
 };
