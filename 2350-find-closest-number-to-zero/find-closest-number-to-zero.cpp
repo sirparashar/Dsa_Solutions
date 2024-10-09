760e7614cf7312db5aaf569a8b266d64360cc4a1
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-10-10 03:04:07 +0530

    Time: 23 ms (16.43%) | Memory: 22.4 MB (90.46%) - LeetSync

--- 2350-find-closest-number-to-zero/find-closest-number-to-zero.cpp
+++ 2350-find-closest-number-to-zero/find-closest-number-to-zero.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int findClosestNumber(vector<int>& nums) {
-        int closest=INT_MAX;
-        sort(nums.begin(),nums.end());
-        for(auto n:nums){
-            if(abs(n-0)<=abs(closest)){
-                closest = n;
+//         setting the ans to maximum value of int
+        int ans = INT_MAX ;
+        for(int i :  nums){
+        // checking if each value of nums is less than the max value
+            if(abs(i) < abs(ans)){
+                ans =  i ; //check for the lesser value
+            }
+            else if(abs(i) == abs(ans)){
+                ans = max (ans,i) ; // return the maximum in cases there are multiple answers
             }
         }
-        return closest;
+        return ans ;
     }
 };
