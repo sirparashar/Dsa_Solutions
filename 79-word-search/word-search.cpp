3ec81b4cde408f591fb67b3892038909373670de
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-10-10 01:48:56 +0530

    Time: 517 ms (57.35%) | Memory: 11.9 MB (50.97%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
+    bool helper(int i, int j, vector<vector<char>>& board, string &word) {
+        if (word.size() == 0) {
+            return true;
+        }
+        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() ||
+            board[i][j] != word[0]) {
+            return false;
+        }
+        // cout<<i;
+
+        char s = board[i][j];
+        board[i][j] = '#';
+        //  return true;
+
+        // cout << i;
+
+        string sub = word.substr(1);
+        bool res =
+            (helper(i + 1, j, board, sub) || helper(i - 1, j, board, sub) ||
+             helper(i, j + 1, board, sub) || helper(i, j - 1, board, sub));
+        // cout << i;
+
+        board[i][j] = s;
+        return res;
+    }
     bool exist(vector<vector<char>>& board, string word) {
-        for (int i=0; i<board.size(); i++){
-            for (int j=0; j<board[0].size(); j++){
-                if(dfs(board,i,j,word)){
+
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                if (helper(i, j, board, word)) {
                     return true;
                 }
             }
         }
         return false;
-    }
-
-    bool dfs(vector<vector<char>>& board,int i, int j, string &word){
-       
-       if (!word.size()){
-           return true;
-       }
-       if (i<0 || i>=board.size() ||j<0||j>=board[0].size()||board[i][j]!=word[0]){
-           return false;
-       }
-
-       char c = board[i][j];
-       board[i][j]='*';
-       string s = word.substr(1);
-       bool ret = dfs(board, i-1, j, s) || dfs(board, i+1, j, s) || dfs(board, i, j-1, s) || dfs(board,i , j+1, s);
-       board[i][j]=c;
-       return ret;
-
     }
 };
