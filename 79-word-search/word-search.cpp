a2cfbbc60050d134d7e76dd1d01f1668d34826f2
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-10-10 01:38:16 +0530

    Time: 514 ms (57.51%) | Memory: 11.7 MB (54.98%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
-        int n = board[0].size();
-        
-        function<bool(int, int, int)> backtrack = [&](int i, int j, int k) {
-            if (k == word.length()) {
-                return true;
-            }
-            if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[k]) {
-                return false;
-            }
-            
-            char temp = board[i][j];
-            board[i][j] = '\0';
-            
-            if (backtrack(i + 1, j, k + 1) || backtrack(i - 1, j, k + 1) || 
-                backtrack(i, j + 1, k + 1) || backtrack(i, j - 1, k + 1)) {
-                return true;
-            }
-            
-            board[i][j] = temp; 
-            return false;
-        };
-        
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (backtrack(i, j, 0)) {
+        for (int i=0; i<board.size(); i++){
+            for (int j=0; j<board[0].size(); j++){
+                if(dfs(board,i,j,word)){
                     return true;
                 }
             }
         }
         return false;
+    }
+
+    bool dfs(vector<vector<char>>& board,int i, int j, string &word){
+       
+       if (!word.size()){
+           return true;
+       }
+       if (i<0 || i>=board.size() ||j<0||j>=board[0].size()||board[i][j]!=word[0]){
+           return false;
+       }
+
+       char c = board[i][j];
+       board[i][j]='*';
+       string s = word.substr(1);
+       bool ret = dfs(board, i-1, j, s) || dfs(board, i+1, j, s) || dfs(board, i, j-1, s) || dfs(board,i , j+1, s);
+       board[i][j]=c;
+       return ret;
+
     }
 };
