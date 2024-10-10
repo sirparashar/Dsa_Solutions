2e7664c1e48e8d39cb463f878490d3e4e0358eeb
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-10-11 04:04:35 +0530

    Time: 57 ms (37.52%) | Memory: 47 MB (25.09%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/maximum-width-ramp.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
-    int maxWidthRamp(vector<int>& A) {
+    int maxWidthRamp(vector<int>& nums) {
         stack<int> s;
-        int res = 0, n = A.size();
-        for (int i = 0; i < n; ++i)
-            if (s.empty() || A[s.top()] > A[i])
+        int res=0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (s.empty() || nums[s.top()] > nums[i]) {
                 s.push(i);
-        for (int i = n - 1; i > res; --i)
-            while (!s.empty() && A[s.top()] <= A[i])
-                res = max(res, i - s.top()), s.pop();
+            }
+        }
+
+        for (int i = nums.size()-1; i >=0 ; i--) {
+           while(!s.empty() && nums[s.top()]<=nums[i]){
+            res= max(res,i-s.top());
+            s.pop();
+           }
+        }
         return res;
+        
     }
 };
