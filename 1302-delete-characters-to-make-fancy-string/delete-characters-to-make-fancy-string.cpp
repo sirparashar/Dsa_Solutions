57e5586e95db4e568c9769c30400fc28d1649a1f
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-11-01 22:33:18 +0530

    Time: 2498 ms (5.19%) | Memory: 42.7 MB (63.26%) - LeetSync

---
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+        int l = 0;
+        int count = 1;
+        string res = "";
+        for (int i = 0; i <= s.size() - 1; i++) {
+            if (s[i] == s[i + 1]) {
+                count++;
+            }
+            else{
+                //cout<<i;
+                count=1;
+            }
+            if (count == 3) {
+                cout<<count;
+                count--;
+                continue;
+            }
+            res += s[i];
+        }
+
+        return res;
+    }
+};
