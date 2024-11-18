081694a3ccb18f04ff5cfcaf289438d7cf2e42c8
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-11-19 00:27:16 +0530

    Time: 75 ms (5.02%) | Memory: 10.4 MB (60.07%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.cpp
+++ 1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -1,36 +1,28 @@
 class Solution {
-private:
-    int forwardCircularSum(vector<int>& vec, int start, int cnt) {
-        int sum = 0;
-        int len = vec.size();
-        for (int i = 0; i < cnt; i++)
-            sum += vec[(start + i) % len];
-        return sum;
-    }
-
-    int backwardCircularSum(vector<int>& vec, int start, int cnt) {
-        int sum = 0;
-        int len = vec.size();
-        for (int i = 0; i < cnt; i++)
-            sum += vec[(start - i - 1 + len) % len];
-        return sum;
-    }
-
 public:
     vector<int> decrypt(vector<int>& code, int k) {
-        vector<int> res(code.size());
-
-        if (k == 0)
+        vector<int> res(code.size(), 0);
+        if (k==0){
             return res;
-
-        else if (k > 0)
-            for (int i = 0; i < code.size(); i++)
-                res[i] = forwardCircularSum(code, i + 1, k);
-        
-        else
-            for (int i = 0; i < code.size(); i++)
-                res[i] = backwardCircularSum(code, i, abs(k));
-
+        }
+        if (k > 0) {
+            for (int i = 0; i < code.size(); i++) {
+                int sum = 0;
+                for (int j = i + 1; j < (k + i + 1); j++) {
+                    sum += code[j % code.size()];
+                }
+                res[i] = sum;
+            }
+        } else {
+            for (int i = 0; i < code.size(); i++) {
+                int sum = 0;
+                for (int j = 0; j < abs(k); j++) {
+                    cout << j;
+                    sum += code[(code.size() - j + i - 1) % code.size()];
+                }
+                res[i] = sum;
+            }
+        }
         return res;
     }
 };
