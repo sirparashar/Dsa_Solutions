c3e44ddd101bdd335d6c827d0d4c626fdf2e4c85
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-12-01 00:52:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (5.86%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    unordered_map<int,int>m;
     int fib(int n) {
         if( n == 1){
             return 1;
@@ -7,6 +8,11 @@ public:
         if( n == 0){
             return 0;
         }
-        return fib(n-1) + fib(n-2);
+        if(m.find(n)!=m.end()){
+            return m[n];
+        }
+        m[n] = fib(n-1) + fib(n-2);
+
+        return m[n];
     }
 };
