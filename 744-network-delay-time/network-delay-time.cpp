5252f7710c492a4ca9b7ef8907899c3a29e49ec6
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-12-08 18:08:16 +0530

    Time: 82 ms (60.43%) | Memory: 44.4 MB (56.98%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,37 +1,40 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>> adj(n+1);
+        vector<vector<pair<int,int>>>adj(n+1);
 
         for(int i =0; i<times.size(); i++){
             adj[times[i][0]].push_back({times[i][1],times[i][2]});
         }
-        vector<int>dist(n+1, INT_MAX);
-        queue<int> q;
 
-        q.push(k);
+        vector<int> dist(n+1,INT_MAX);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+
+        pq.push({0,k});
         dist[k]=0;
+        while(!pq.empty()){
+            auto p = pq.top();
+            pq.pop();
 
-        while(!q.empty()){
-            int t = q.front();
-            q.pop();
-
-            for(auto pair : adj[t]){
-                if(dist[pair.first]>dist[t]+pair.second){
-                    dist[pair.first] = dist[t]+pair.second;
-                    q.push(pair.first);
+            for(auto i : adj[p.second]){
+                if(dist[i.first]>p.first+i.second){
+                    dist[i.first]=p.first+i.second;
+                    pq.push({dist[i.first],i.first});
                 }
             }
         }
 
-        int res =0; 
-        for(int i =1; i<=n; i++){
+        int res=0; 
+
+        for(int i=1; i<=n; i++){
             if(dist[i]==INT_MAX){
                 return -1;
             }
             res = max(res,dist[i]);
         }
         return res;
- 
+
+
+
     }
 };
