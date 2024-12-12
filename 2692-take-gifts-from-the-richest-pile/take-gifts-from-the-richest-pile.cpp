1b189fc623f34e984b36ca3512de965b76b6e69a
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-12-13 00:26:29 +0530

    Time: 88 ms (5.15%) | Memory: 13 MB (89.48%) - LeetSync

---
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        for(int i =0; i<k; i++){
+            sort(gifts.begin(), gifts.end(),greater<int>());
+            gifts[0]=floor(sqrt(gifts[0]));
+        }
+        long long sum=0;
+        for(auto i :gifts){
+            cout<<i<<" ";
+            sum+=i;
+        }
+        return sum;
+    }
+};
