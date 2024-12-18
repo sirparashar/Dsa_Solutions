d4316a476c3669d323ad2db77b34b022fc632327
sirparashar <53056784+sirparashar@users.noreply.github.com>
2024-12-18 23:45:02 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (15.50%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.cpp
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -6,33 +6,31 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-      
-    void getpaths(TreeNode* root, vector<string>&res, string str ){
-        if(root==nullptr){
-            return ;
-        }
-        if(root->left==nullptr && root->right==nullptr){
-            str+=to_string(root->val);
-            res.push_back(str);
-            str="";
+    vector<string> res;
+    void getpaths(TreeNode* root, string str) {
+        if (root == nullptr) {
             return;
         }
-          
-         str+=to_string(root->val)+"->";
-         getpaths(root->left,res,str);
-         getpaths(root->right,res,str);
+        if (root->left == nullptr && root->right == nullptr) {
+            str += to_string(root->val);
+            res.push_back(str);
+            str = "";
+            return;
+        }
 
-
+        str += to_string(root->val) + "->";
+        getpaths(root->left, str);
+        getpaths(root->right, str);
     }
 
     vector<string> binaryTreePaths(TreeNode* root) {
-        vector<string>res;
-        getpaths(root,res,"");
+        getpaths(root, "");
         return res;
     }
 };
